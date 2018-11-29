36131e54c07a7ee60ce7d328a840ec1db3ae270a
Marek Stevuliak <xstevuliakm@stuba.sk>
2018-11-30 00:27:44 +0100

    prepared safe libraries query

--- src/main/java/sk/mste/pdtproject/util/QueryTemplates.java
+++ src/main/java/sk/mste/pdtproject/util/QueryTemplates.java
@@ -27,5 +27,30 @@ public final class QueryTemplates {
             .append(" limit 1200")
             .toString();
 
+    public static final String QUERY_SAFE_LIBS = new StringBuilder()
+    .append("  with libraries as (")
+    .append("      select *, ")
+    .append("         st_asgeojson(ST_Transform(p.way, 4326)),")
+    .append("         round(")
+    .append("            cast(ST_Distance(ST_MakePoint(?, ?)::geography,")
+    .append("                             ST_Transform(p.way, 4326)::geography) as numeric),")
+    .append("                             0")
+    .append("         ) as distance")
+    .append("       from planet_osm_point p")
+    .append("       where 1=1")
+    .append("           and p.amenity ='library'")
+    .append("           order by distance limit 10")
+    .append("           )")
+    .append("       select l.name, ")
+    .append("              (")
+    .append("                   select count(*) ")
+    .append("                   from fires f ")
+    .append("                   where ")
+    .append("                       ST_DWithin(ST_Transform(l.way,4326), ST_Transform(f.way,4326)::geography, 500)) ")
+    .append("              as fire_density, ")
+    .append("              l.st_asgeojson ")
+    .append("       from libraries l")
+    .append("       order by fire_density").toString();
+
     private QueryTemplates() {}
 }
